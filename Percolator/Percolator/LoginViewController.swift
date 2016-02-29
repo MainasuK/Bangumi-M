a7c24bf9017f65e6f623ddd0252212e282f1ed19
CMK <cirno.mainasuk@gmail.com>
2016-02-29 23:31:05 +0800

    refactor(LoginViewController.swift): add final keyword

--- Percolator/Percolator/LoginViewController.swift
+++ Percolator/Percolator/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
     
     let myKeyChainWrapper = KeychainWrapper()
     var delegate: MenuTransitionDelegate?
