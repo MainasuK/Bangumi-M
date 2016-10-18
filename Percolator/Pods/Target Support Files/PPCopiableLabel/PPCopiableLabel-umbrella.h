87725ef8d7ea0b8b73d41070193bed1e3d6eda8a
CMK <cirno.mainasuk@gmail.com>
2016-10-18 16:47:15 +0800

    chore: add PPCopiableLabel pod

---
+++ Percolator/Pods/Target Support Files/PPCopiableLabel/PPCopiableLabel-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "PPCopiableLabel.h"
+
+FOUNDATION_EXPORT double PPCopiableLabelVersionNumber;
+FOUNDATION_EXPORT const unsigned char PPCopiableLabelVersionString[];
+
